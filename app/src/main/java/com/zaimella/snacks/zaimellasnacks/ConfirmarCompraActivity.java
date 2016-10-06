e5306c4f8de3509be0f5c100bb8c4baa6a934876
Mauro Velasco <mauro.velasco@gmail.com>
2016-10-06 15:01:58 -0500

    Proceso de compra

--- app/src/main/java/com/zaimella/snacks/zaimellasnacks/ConfirmarCompraActivity.java
+++ app/src/main/java/com/zaimella/snacks/zaimellasnacks/ConfirmarCompraActivity.java
@@ -1,13 +1,54 @@
 package com.zaimella.snacks.zaimellasnacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zaimella.log.Logger;
+import com.zaimella.snacks.service.Constantes;
 
 public class ConfirmarCompraActivity extends AppCompatActivity {
 
+    Logger logger;
+
+    private TextView mNombrePersona;
+    private TextView mValorCompra;
+    private TextView mObservacionesCompra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_compra);
+
+        mNombrePersona = (TextView) findViewById(R.id.idNombrePersona);
+        mValorCompra = (TextView) findViewById(R.id.idValorCompra);
+        mObservacionesCompra = (TextView) findViewById(R.id.idObservacionesCompra);
+
+        Intent intent = getIntent();
+        logger.addRecordToLog("idUsuarioAratek : " + intent.getIntExtra( Constantes.ID_USUARIO_ARATEK , 0 ));
+        logger.addRecordToLog("valorCompra : " + intent.getFloatExtra( Constantes.VALOR_COMPRA , 0  ) );
+        logger.addRecordToLog("observaciones : " + intent.getStringExtra( Constantes.OBSERVACIONES ));
+
+        mNombrePersona.setText( Integer.toString( intent.getIntExtra( Constantes.ID_USUARIO_ARATEK , 0 ) ) );
+        mValorCompra.setText( Float.toString( intent.getFloatExtra( Constantes.VALOR_COMPRA , 0  ) ) );
+        mObservacionesCompra.setText( intent.getStringExtra( Constantes.OBSERVACIONES ) );
+
     }
+
+    //public void btnRegistrarHuella(View view){
+    public void btnConfirmarCompra(View view){
+
+    }
+
+    public void btnCancelarCompra(View view){
+
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
 }
